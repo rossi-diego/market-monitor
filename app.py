1da8d5c34b7f3bff3396e72cd3afc929dc5736ec
Diego Rossi <94.diegorossi@gmail.com>
2025-09-05 10:20:27 -0300

    ajustando pages

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ import streamlit as st
 
 st.set_page_config(page_title="Market Monitor", page_icon="📊", layout="wide")
 
-pg_home     = st.Page("pages/1_Home.py", title="Home", icon="🏠")
-pg_virtual  = st.Page("pages/2_Virtual.py", title="Home", icon="📊")
-pg_analise  = st.Page("pages/3_Analise_de_ativos.py", title="Análise de ativos", icon="📈")
+pg_home     = st.Page("pages/1_Home.py",                 title="Home",                  icon="🏠")
+pg_virtual  = st.Page("pages/2_Virtual.py",              title="Virtual",               icon="📊")
+pg_analise  = st.Page("pages/3_Analise_de_ativos.py",    title="Análise de ativos",     icon="📈")
 pg_rel      = st.Page("pages/4_Relacoes_de_arbitragem.py", title="Relações de arbitragem", icon="🔁")
-pg_corr     = st.Page("pages/5_Correlacoes.py", title="Correlações", icon="🧩")
-pg_wasde    = st.Page("pages/6_WASDE_reports.py", title="WASDE Reports", icon="📄")
-pg_ml       = st.Page("pages/7_Machine_Learning.py", title="Machine Learning", icon="🤖")
+pg_corr     = st.Page("pages/5_Correlacoes.py",          title="Correlações",           icon="🧩")
+pg_wasde    = st.Page("pages/6_WASDE_reports.py",        title="WASDE reports",         icon="📄")
+pg_ml       = st.Page("pages/7_Machine_Learning.py",     title="Machine Learning",      icon="🤖")
 
 nav = st.navigation({
-    "Menu": [pg_home, pg_analise, pg_rel, pg_corr, pg_wasde, pg_ml]
+    "Menu": [pg_home, pg_virtual, pg_analise, pg_rel, pg_corr, pg_wasde, pg_ml]
 })
 nav.run()
